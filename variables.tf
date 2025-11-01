ffcdd2576a235214aec0211a36a1de0f442c7f9b
donovanreid2 <85472751+donovanreid2@users.noreply.github.com>
2025-11-01 15:07:11 -0700

    Refactor variables.tf and add new variable definitions
    Removed commented-out variables and added new variables for short description and resource name overrides.

--- variables.tf
+++ variables.tf
@@ -1,85 +1,59 @@
-# variable "owner" {
-#   default = "__owner__"
-# }
-
-# variable "backup" {
-#   default = "__backup__"
-#   type = string
-# }
-
-# variable "application" {
-#   default = "__application__"
-#   type = string
-
-# }
-
-# variable "scheduler" {
-#   default = "__scheduler__"
-#   type = string
-
-# }
-
-# variable "tech_lob" {
-#   default = "__tech_lob__"
-#   type = string
-
-# }
-
-#########These are for RG###########
-variable "provisioned_by" { #done
+variable "provisioned_by" {
   default = "__provisioned_by__"
   type    = string
-
 }
 
 variable "revenue" {
   default = "__revenue__"
   type    = string
-
 }
 
 variable "app_id" {
   default = "__app_id__"
   type    = string
-
 }
 
 variable "app_name" {
   default = "__app_name__"
   type    = string
-
 }
 
 variable "app_tier" {
   default = "__app_tier__"
   type    = string
-
 }
 
 variable "support_team" {
   default = "__support_team__"
   type    = string
-
 }
-
 
 variable "resource_group_owner" {
   default = "__tech_lob__"
   type    = string
-
 }
 
+# NEW: short description to put in the name
+variable "shortdesc" {
+  type    = string
+  default = ""
+  description = "Short description of what the resource/group is for. Will be sanitized to [a-z0-9-] and truncated."
+  validation {
+    condition     = length(var.shortdesc) <= 40
+    error_message = "shortdesc must be 40 characters or less."
+  }
+}
 
-
-#general
+# general
 variable "location" {
+  type = string
 }
 
 variable "generator" {
   type = map(map(number))
   default = {
-    "app" = {
-      "rg" = 1
+    app = {
+      resource_group = 1
     }
   }
   validation {
@@ -97,4 +71,18 @@ variable "generator" {
     )
     error_message = "Invalid input for 'generator' variable."
   }
+}
+
+# NEW: per-resource overrides to deal with Azure name length / permitted chars
+# Example:
+# resource_name_overrides = {
+#   storage_account = { max_length = 24, strip_hyphens = true }
+# }
+variable "resource_name_overrides" {
+  type = map(object({
+    max_length   = number
+    strip_hyphens = bool
+  }))
+  default = {}
+  description = "Optional per-resource overrides to handle Azure character or length limits."
 }
