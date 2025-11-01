78f87375c070469c4ac01aa9bc854f44af591916
donovanreid2 <85472751+donovanreid2@users.noreply.github.com>
2025-11-01 19:49:32 -0400

    Update examples.tf

--- examples.tf
+++ examples.tf
@@ -1,4 +1,4 @@
-Error: Invalid reference
+ Error: Invalid reference
 │
 │   on locals.tf line 128, in locals:
 │  128:         name = is_alphanumeric ? (
@@ -54,3 +54,77 @@ Error: Invalid reference
 │     │ local.shortdesc_raw is a string
 │
 │ There is no function named "regexreplace".
+╵
+PS C:\Users\reidd3\Documents\moodysterraform\level3\acr> terraform plan
+╷
+│ Error: Invalid reference
+│
+│   on locals.tf line 128, in locals:
+│  128:         name = is_alphanumeric ? (
+│
+│ A reference to a resource type must be followed by at least one attribute access, specifying the resource name.  
+╵
+╷
+│ Error: Invalid reference
+│
+│   on locals.tf line 129, in locals:
+│  129:           "${local.naming_components.business}${resource_abbr}${local.naming_components.business_unit}${local.naming_components.short_description}${local.naming_components.environment}${region_code}${local.naming_components.sequence}"
+│
+│ A reference to a resource type must be followed by at least one attribute access, specifying the resource name.  
+╵
+╷
+│ Error: Invalid reference
+│
+│   on locals.tf line 129, in locals:
+│  129:           "${local.naming_components.business}${resource_abbr}${local.naming_components.business_unit}${local.naming_components.short_description}${local.naming_components.environment}${region_code}${local.naming_components.sequence}"
+│
+│ A reference to a resource type must be followed by at least one attribute access, specifying the resource name.  
+╵
+╷
+│ Error: Invalid reference
+│
+│   on locals.tf line 131, in locals:
+│  131:           "${local.naming_components.business}-${resource_abbr}-${local.naming_components.business_unit}-${local.naming_components.short_description}-${local.naming_components.environment}-${region_code}-${local.naming_components.sequence}"
+│
+│ A reference to a resource type must be followed by at least one attribute access, specifying the resource name.  
+╵
+╷
+│ Error: Invalid reference
+│
+│   on locals.tf line 131, in locals:
+│  131:           "${local.naming_components.business}-${resource_abbr}-${local.naming_components.business_unit}-${local.naming_components.short_description}-${local.naming_components.environment}-${region_code}-${local.naming_components.sequence}"
+│
+│ A reference to a resource type must be followed by at least one attribute access, specifying the resource name.  
+╵
+╷
+│ Error: Invalid reference
+│
+│   on locals.tf line 134, in locals:
+│  134:         final_name = lower(name)
+│
+│ A reference to a resource type must be followed by at least one attribute access, specifying the resource name.  
+╵
+╷
+│ Error: Invalid reference
+│
+│   on modules\naming\main.tf line 47, in locals:
+│   47:   shortdesc_clean_1 = replace(shortdesc_lower, "/[^a-z0-9-]/", "")
+│
+│ A reference to a resource type must be followed by at least one attribute access, specifying the resource name.  
+╵
+╷
+│ Error: Invalid reference
+│
+│   on modules\naming\main.tf line 48, in locals:
+│   48:   shortdesc_clean_2 = replace(shortdesc_clean_1, "/-+/", "-")
+│
+│ A reference to a resource type must be followed by at least one attribute access, specifying the resource name.  
+╵
+╷
+│ Error: Invalid reference
+│
+│   on modules\naming\main.tf line 49, in locals:
+│   49:   shortdesc         = substr(shortdesc_clean_2, 0, 20)
+│
+│ A reference to a resource type must be followed by at least one attribute access, specifying the resource name.  
+╵
