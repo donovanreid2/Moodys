f886a4ba9229b7d397c7fda81443459be319e069
donovanreid2 <85472751+donovanreid2@users.noreply.github.com>
2025-11-01 18:27:22 -0700

    Modify outputs to use 'try' for resource names
    Updated output values to use 'try' for safer access.

--- outputs.tf
+++ outputs.tf
@@ -3,21 +3,20 @@ output "resources" {
   value       = local.resources_first
 }
 
-# optional shortcuts
 output "resource_group" {
-  value = local.resources_first.resource_group
+  value = try(local.resources_first.resource_group.name, null)
 }
 
 output "storage_account" {
-  value = local.resources_first.storage_account
+  value = try(local.resources_first.storage_account.name, null)
 }
 
 output "key_vault" {
-  value = local.resources_first.key_vault
+  value = try(local.resources_first.key_vault.name, null)
 }
 
 output "container_registry" {
-  value = local.resources_first.container_registry
+  value = try(local.resources_first.container_registry.name, null)
 }
 
 output "generated_names" {
